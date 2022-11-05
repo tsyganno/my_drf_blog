79a29bc71c126ffebdde8847ef64e2166f5ebd91
tsyganno <tsyganlee@gmail.com>
2022-11-05 22:53:46 +0800

    add pagination

--- core/views.py
+++ core/views.py
@@ -4,10 +4,19 @@ from .models import Post
 from rest_framework import permissions
 from rest_framework.response import Response
 
+from rest_framework import pagination
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
 
